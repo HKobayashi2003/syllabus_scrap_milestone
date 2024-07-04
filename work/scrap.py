1943373a3a92d7aac31342025d816730e342d9a7
Takaki Kaito <nbtmktrt33312@gmail.com>
2024-07-04 13:11:18 +0900

    変更点 ・ライブラリ変更によりスクレイピング時間の短縮 ・エラー処理の追加 ・実行ログを見やすく変更 ・windows上での実行における改行コードの問題の対処 ・一部関数のリファクタリング

--- work/scrap.py
+++ work/scrap.py
@@ -1,5 +1,9 @@
+import os
 import csv
 import datetime
+import time
+import subprocess
+from logging import getLogger, handlers, Formatter, DEBUG, ERROR
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
@@ -7,8 +11,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from mojimoji import zen_to_han
-import subprocess
-from logging import getLogger, handlers, Formatter, DEBUG, ERROR
+from tqdm import tqdm
+from bs4 import BeautifulSoup
 
 faculties = ['基幹', '創造', '先進', '政経', '法学', '教育', '商学', '社学', '人科', 'スポーツ', '国際教養', '文構', '文', '人通', '芸術', '日本語', '留学', 'グローバル']
 
@@ -16,19 +20,14 @@ faculties = ['基幹', '創造', '先進', '政経', '法学', '教育', '商学
 def set_logger():
     # 全体のログ設定
     # ファイルに書き出す。ログが100KB溜まったらバックアップにして新しいファイルを作る。
-    root_logger = getLogger()
-    root_logger.setLevel(DEBUG)
-    rotating_handler = handlers.RotatingFileHandler(
-        r'./app.log',
-        mode="a",
-        maxBytes=100 * 1024,
-        backupCount=3,
-        encoding="utf-8"
+    logger = getLogger()
+    logger.setLevel(DEBUG)
+    handler = handlers.RotatingFileHandler(
+        './app.log', maxBytes=100 * 1024, backupCount=3, encoding='utf-8'
     )
-    format = Formatter('%(asctime)s : %(levelname)s : %(message)s')
-    rotating_handler.setFormatter(format)
-    root_logger.addHandler(rotating_handler)
-
+    formatter = Formatter('%(asctime)s : %(levelname)s : %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
     block_logger = getLogger()
     block_logger.setLevel(ERROR)  # DEBUGやINFOなどのレベルのログを無視
     main_logger = getLogger("__main__")
@@ -45,9 +44,7 @@ def log(arg, level=DEBUG):
 
 def get_current():
     now = datetime.datetime.now()
-    year = now.year
-    month = now.month
-    return year, month
+    return now.year, now.month
 
 def init_csv_file(year, term):
     with open(f'../data/syllabus_data_{year}_{term}.csv', 'a', newline='', encoding='utf-8') as file:
@@ -55,41 +52,50 @@ def init_csv_file(year, term):
         writer.writerow(['学部', 'コース名', '開講学期', '教員名', '曜日時限'])
 
 def scrape_syllabus_data(driver, dest):
+    log("Accessing Waseda's syllabus\n")
     driver.get('https://www.wsl.waseda.jp/syllabus/JAA101.php')
     with open(dest, 'w', newline='', encoding='utf-8') as file:
         writer = csv.writer(file)
-        for name in faculties:
+        for name in tqdm(faculties, desc="Faculty", dynamic_ncols=True):
+            log(f'Accessing {name} syllabus')
             select = Select(driver.find_element(By.NAME, 'p_gakubu'))
             select.select_by_visible_text(name)
-            driver.find_element(By.NAME, 'btnSubmit').click()
-            log(f"Scraping {name} data.")
+            driver.execute_script("func_search('JAA103SubCon');")
+            driver.execute_script("func_showchg('JAA103SubCon', '1000');")
 
+            log(f"Scraping {name} data.")
+            start_time = time.time()
+            total_elements = 0
             while True:
                 try:
-                    for i in range(10):
-                        course_name = driver.find_element(By.XPATH, f'//*[@id="cCommon"]/div/div/div/div/div[1]/div[2]/table/tbody/tr[{i+2}]/td[3]/a').text
-                        semester_name = driver.find_element(By.XPATH, f'//*[@id="cCommon"]/div/div/div/div/div[1]/div[2]/table/tbody/tr[{i+2}]/td[6]').text
-                        teacher_name = driver.find_element(By.XPATH, f'//*[@id="cCommon"]/div/div/div/div/div[1]/div[2]/table/tbody/tr[{i+2}]/td[4]').text
-                        week_name = driver.find_element(By.XPATH, f'//*[@id="cCommon"]/div/div/div/div/div[1]/div[2]/table/tbody/tr[{i+2}]/td[7]').text
-
-                        writer.writerow([name, course_name, semester_name, teacher_name, week_name])
-
-                    driver.find_element(By.XPATH, '//*[@id="cHonbun"]/div[2]/table/tbody/tr/td[3]/div/div/p/a').click()
+                    soup = BeautifulSoup(driver.page_source, 'html.parser')
+                    rows = soup.select(
+                        '#cCommon div div div div div:nth-child(1) div:nth-child(2) table tbody tr')
+                    total_elements += len(rows[1:])
+                    for row in rows[1:]:
+                        cols = row.find_all('td')
+                        writer.writerow([
+                            name,
+                            cols[2].text.strip(),
+                            cols[5].text.strip(),
+                            cols[3].text.strip(),
+                            cols[6].text.strip()
+                        ])
+                    driver.find_element(
+                        By.XPATH, '//*[@id="cHonbun"]/div[2]/table/tbody/tr/td[3]/div/div/p/a').click()
                 except NoSuchElementException:
                     break
-
+            log(f'Total Number of Subjects {name}: {total_elements}')
+            log(f'Finished in {time.time() - start_time:.6f} seconds\n')
             driver.find_element(By.CLASS_NAME, 'ch-back').click()
 
 def convert_zen_to_han(source_path, output_path):
-    converted_data = []
-    with open(source_path, 'r', newline='', encoding='utf-8') as file:
-        csvreader = csv.reader(file)
-        for row in csvreader:
-            converted_row = [zen_to_han(cell, kana=False) for cell in row]
-            converted_data.append(converted_row)
-    with open(output_path, 'w', newline='', encoding='utf-8') as file:
-        csvwriter = csv.writer(file)
-        csvwriter.writerows(converted_data)
+    with open(source_path, 'r', newline='', encoding='utf-8') as infile, open(output_path, 'w', newline='', encoding='utf-8') as outfile:
+        reader = csv.reader(infile)
+        writer = csv.writer(outfile)
+        for row in reader:
+            writer.writerow([zen_to_han(cell, kana=False) for cell in row])
+
 
 def process_schedule1(row):
     schedule = str(row[4])
@@ -140,7 +146,8 @@ def format_syllabus_data(source_path, dest_path):
         csvreader = csv.reader(source)
         csvwriter = csv.writer(dest)
         log("Formatting data.")
-        for row in csvreader:
+        rows = list(csvreader)
+        for row in tqdm(rows, desc="Formatting data", leave=False):
             try:
                 han_row = [zen_to_han(cell, kana=False) for cell in row]
                 fmt1 = process_schedule1(han_row)
@@ -149,27 +156,34 @@ def format_syllabus_data(source_path, dest_path):
                     for final_row in fmt2:
                         csvwriter.writerow(final_row)
             except Exception as e:
-                log(e, level=ERROR)
-                log(row, level=ERROR)
+                log(f"Error processing row: {row} - {e}", ERROR)
+
 
 def main():
     log("==========Scraping started==========")
-    check_versions()
-    year, month = get_current()
-    dest_dir = '../data'
-    name = f'syllabus_data_{year}_{month}.csv'
-    raw_dir = f'{dest_dir}/raw_{name}'
-    out_dir = f'{dest_dir}/{name}'
+    start_time = time.time()
+    try:
+        check_versions()
+        year, month = get_current()
+        dest_dir = '../data'
+        raw_dir = f'{dest_dir}/raw_syllabus_data_{year}_{month}.csv'
+        out_dir = f'{dest_dir}/syllabus_data_{year}_{month}.csv'
 
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--verbose")
+        os.makedirs(dest_dir, exist_ok=True)
 
-    chrome_service = Service('/usr/local/bin/chromedriver')
-    driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--verbose")
+
+        driver = webdriver.Remote(
+            command_executor='http://selenium:4444/wd/hub',
+            options=chrome_options
+        )
+    except Exception as e:
+        log(f"Error : {e}", ERROR)
 
     try:
         scrape_syllabus_data(driver, raw_dir)
@@ -177,7 +191,7 @@ def main():
         driver.quit()
 
     format_syllabus_data(raw_dir, out_dir)
-
+    log(f'Total Execution Time {time.time() - start_time:.6f} seconds')
     log("==========Scraping completed==========")
 
 if __name__ == "__main__":
